faae204d0b1590db483129342326d04e017d888e
Tyler Harris <tylerdavidharris@gmail.com>
2016-02-04 20:17:50 -0700

    live edit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express'),
-	router = express.Router();
+	router = express.Router(),
+	jsonUtil = require('./json-util.js'),
+	path = require('path');
+
+var portfolioData = path.join(__dirname, '../data/portfolios.json');
+var	contentData = path.join(__dirname, '../data/handbook.json');
+
 
 /* GET home page. */;
 
@@ -7,6 +13,47 @@ router.get('/', function(req, res) {
   res.render('index', { 
   	title: 'handbook demo' 
   });
+});
+
+router.get('/getPortfolioData', function(req, res) {
+	jsonUtil.loadJson(portfolioData, function(err, results) {
+		if(err) console.log(err);
+		res.send(results);
+	});
+});
+
+router.get('/getContentData', function(req, res) {
+	console.log('retrieving', contentData);
+	jsonUtil.loadJson(contentData, function(err, results) {
+		if(err) console.log(err);
+		res.send(results);
+	});
+});
+
+router.post('/editContentData', function(req, res) {
+	jsonUtil.loadJson(contentData, function (err, json) {
+	    // TODO: make sure you handle errors
+	    // if err is not null, you can either consider it an error, or
+	    // you could simply say json = [] and start a new file
+	    if(err) {
+	    	console.log(err);
+	    	res.send(500, 'Problem saving data: ' + err);
+	    }
+
+	    else {
+	    	// should also do validation checks like if(json instanceof Array) and
+		    // verify that req.body exists and is properly formatted, etc
+
+		    json = req.body;
+
+		    // re-save the file
+		    jsonUtil.writeJson(contentData, json, function (err) {
+		      if (err) console.log(err);
+		      else res.sendStatus(200);
+		    });
+		}
+	});
+
 });
 
 module.exports = router;
---
+++ routes/json-util.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+module.exports.loadJson = function (file, callback) {
+  fs.readFile(file, { encoding: 'utf8' }, function (err, data) {
+    if (err) return callback(err); // file reading error
+    try {
+      // parse and return json to callback
+      var json = JSON.parse(data);
+      callback(null, json);
+    } catch (ex) {
+      // catch JSON parsing errors so your app doesn't crash
+      callback(ex);
+    }
+  });
+};
+
+module.exports.writeJson = function (file, json, callback) {
+  fs.writeFile(file, JSON.stringify(json), callback);
+};
